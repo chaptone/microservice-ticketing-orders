7eb2831954e8f9c68df60feecea8e70137ed28f1
chaptone <rakkan.j@gmail.com>
2021-02-07 20:05:19 +0700

    Added: test

--- src/routes/show.ts
+++ src/routes/show.ts
@@ -1,9 +1,37 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@rkktickets/common";
+import { Order } from "../models/order";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req, res) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  [
+    body("orderId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("OrderId must ve provided"),
+  ],
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  },
+);
 
 export { router as showOrderRouter };
